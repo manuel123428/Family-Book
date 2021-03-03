425123c5eca7213c21848b4fd156b9dacf551071
manuel123428 <76941019+manuel123428@users.noreply.github.com>
2021-03-03 23:00:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var images =["manu_profile.jpg","prithi.jpg","Sam.jpg","danu.jpg"];
+var names=["Manuel Reginald","Prithi","Samuel","Daniel"];
+
+var i =0;
+function NEXT(){
+    document.getElementById("p").innerHTML=names[i];
+    document.getElementById("img").src=images[i];
+    i++;
+}
+
+
+
+
+
+
+
+
+
+
